81be9c7b4e23bb2ad7cb1a99f87789d4ff2de4d6
sajjadulislam112710 <sajjadulislam112710@gmail.com>
2026-06-16 19:40:53 +0600

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cashometry.app',
+  appName: 'cashometry',
+  webDir: 'dist'
+};
+
+export default config;
